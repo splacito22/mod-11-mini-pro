e68c3c38d4f0e6c5d73415afa65c5d9ac8034b11
Stephany <136421961+splacito22@users.noreply.github.com>
2023-09-25 09:09:19 -0700

    implemented diagnostic route for api

--- Develop/routes/diagnostics.js
+++ Develop/routes/diagnostics.js
@@ -1,15 +1,37 @@
-const diagnostics = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const diagnostics = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
 
+// constants
+const FILE_NAME = "./db/diagnostics.json";
 // GET Route for retrieving diagnostic information
-diagnostics.get('/', (req, res) => {
-  // TODO: Logic for sending all the content of db/diagnostics.json
+diagnostics.get("/", (req, res) => {
+  readFromFile(FILE_NAME).then((data) => {
+    res.json(JSON.parse(data));
+  });
 });
 
 // POST Route for a error logging
-diagnostics.post('/', (req, res) => {
-  // TODO: Logic for appending data to the db/diagnostics.json file
+diagnostics.post("/", (req, res) => {
+  console.log(req.body);
+
+  const { isValid, errors } = req.body;
+
+  const payload = {
+    time: Date.now(),
+    error_id: uuidv4(),
+    errors,
+  };
+
+  if (!isValid) {
+    readAndAppend(payload, FILE_NAME);
+    res.json(`Diagnostic information added 🔧`);
+  } else {
+    res.json({
+      message: "Object is valid, not logging. Check front end implementation.",
+      error_id: payload.error_id,
+    });
+  }
 });
 
 module.exports = diagnostics;
